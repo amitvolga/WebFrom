37683625dc2fb81b204c1d6dd6fa07f07e7c4fdf
amitvolga <amit.shimon@gmail.com>
2018-05-31 00:35:26 +0300

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplication1.Login" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="imgcontainer">
+   <h1>התחבר למערכת</h1>
+        <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
+    <div class="container">
+        <p><%=loginAttem%>:נסיונות התחברות</p>
+        <div class="form-group">
+            <asp:Label AssociatedControlID="UserName" runat="server">User Name</asp:Label>
+            <asp:TextBox ID="UserName" CssClass="inputText" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <asp:Label AssociatedControlID="Password" runat="server">PassWord</asp:Label>
+            <asp:TextBox ID="Password" AutoPostBack="false" CssClass="inputText" TextMode="Password" runat="server"></asp:TextBox>
+        </div>
+         <div style="margin-top: 20px;">
+            <asp:Button ID="LoginSubmit" CssClass="buttons" Text="Submit"  OnClick="LoginSubmit_Click" runat="server" />
+            <asp:Label ID="LoginLabel" runat="server"></asp:Label>
+        </div>
+        <br />
+    </div>
+
+</asp:Content>
---
+++ Login.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    
+    public partial class Login : System.Web.UI.Page
+    {
+        public MyAdoHelperAccess myAdoHelperAccess = null;
+        public static string userName = string.Empty;
+        public static int loginAttem = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void LoginSubmit_Click(object sender, EventArgs e)
+        {
+            bool isExsist = false;
+            var filename = string.Empty;
+            myAdoHelperAccess = new MyAdoHelperAccess();
+            if (UserName.Text != "" || Password.Text != "")
+            {
+                var sqlComm = "Select * From tbl_user where userName='" + UserName.Text + "' and password ='" + Password.Text + "'";
+
+                    isExsist= myAdoHelperAccess.IsExist(filename, sqlComm);
+                if (isExsist)
+                {
+                    loginAttem = 1;
+                    Response.Redirect("HtmlPage2.html");
+                }
+                else
+                {
+                    if (userName == UserName.Text)
+                    {
+                        loginAttem++;
+                    }
+                    userName = UserName.Text;
+                    Response.Redirect("Login.aspx");
+                }
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+    }
+}
