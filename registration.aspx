16120cd6de45d2a41f4a30310fc75d63d0b01015
amitvolga <amit.shimon@gmail.com>
2018-05-31 00:37:03 +0300

    Add files via upload

---
+++ registration.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="registration.aspx.cs" Inherits="WebApplication1.registration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+      <h1>הרשמה</h1>
+        <div class="">
+            <asp:Label AssociatedControlID="FirstName" runat="server">FirstName</asp:Label>
+            <asp:TextBox ID="FirstName" CssClass="inputText" runat="server"></asp:TextBox>
+        </div>
+        <div class="">
+            <asp:Label AssociatedControlID="LastName" runat="server">LastName</asp:Label>
+            <asp:TextBox ID="LastName" CssClass="inputText" runat="server"></asp:TextBox>
+        </div>
+        <div class="">
+            <asp:Label AssociatedControlID="DateOfbirth" runat="server">Date Of Birth</asp:Label>
+            <asp:Calendar ID="DateOfbirth" AutoPostBack="false" runat="server"></asp:Calendar>
+        </div>
+        <div class="">
+            <asp:Label AssociatedControlID="UserName" runat="server">UserName</asp:Label>
+            <asp:TextBox ID="UserName" CssClass="inputText" runat="server"></asp:TextBox>
+        </div>
+        <div class="">
+            <asp:Label AssociatedControlID="Email" runat="server">Email Address</asp:Label>
+            <asp:TextBox ID="Email" CssClass="inputText" TextMode="Email" runat="server"></asp:TextBox>
+        </div>
+        <div class="">
+            <asp:Label AssociatedControlID="Password" runat="server">Password</asp:Label>
+            <asp:TextBox ID="Password" AutoPostBack="false"  CssClass="inputText"  TextMode="Password" runat="server"></asp:TextBox>
+        </div>
+        <div class="">
+            <asp:Label AssociatedControlID="RePassword" runat="server">Secound PassWord</asp:Label>
+            <asp:TextBox ID="RePassword" AutoPostBack="false"  CssClass="inputText"  TextMode="Password" runat="server"></asp:TextBox>
+        </div>
+        <div class="">
+            <asp:Label AssociatedControlID="List" runat="server">משתמש לפי ID</asp:Label>
+            <asp:DropDownList ID="List"
+                AutoPostBack="true"
+                runat="server"  onselectedindexchanged="GetUsersById">
+                <asp:ListItem Selected="True" Value="ID"> </asp:ListItem>
+                <asp:ListItem Value="1">id 1</asp:ListItem>
+                <asp:ListItem Value="2">id 2</asp:ListItem>
+                <asp:ListItem Value="3">id 3</asp:ListItem>
+                <asp:ListItem Value="4">id 4</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <div class="">
+            <asp:CheckBox ID="Approval" CssClass="checkbox"
+                AutoPostBack="false"
+                Text="Approval"
+                TextAlign="Right"
+                runat="server" />
+        </div>
+        <div class="">
+            <asp:RadioButton ID="Male" GroupName="RegularMenu"
+                Text="Male" BackColor="Blue" runat="server" CssClass="inline-rb" />
+            <asp:RadioButton ID="Female" GroupName="RegularMenu"
+                Text="Female" BackColor="Red" runat="server" CssClass="inline-rb" />
+        </div>
+        <div style="margin-top: 20px;">
+            <asp:Button ID="RegisrationSubmit" CssClass="buttons" Text="Submit"  OnClick="RegisrationSubmit_Click" runat="server" />
+            <asp:Label ID="RegisrationLabel" runat="server"></asp:Label>
+        </div>
+        <div style="margin-top: 20px;">
+            <asp:Button ID="Rest" CssClass="buttons" Text="Reset"  OnClick="ResetButton_Click" runat="server" />
+            <asp:Label ID="LabelReset" runat="server"></asp:Label>
+        </div>
+        <br />
+    </div>
+</asp:Content>
---
+++ registration.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Data;
+
+namespace WebApplication1
+{
+
+    public partial class registration : System.Web.UI.Page
+    {
+        public MyAdoHelperAccess myAdoHelperAccess = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private RegistrationForm registaration()
+        {
+            RegistrationForm registrationForm = new RegistrationForm();
+            if ((Password.Text != "" && RePassword.Text != "") && (Password.Text == RePassword.Text))
+            {
+                registrationForm.Password = Password.Text;
+
+                if (Approval.Text != "")
+                {
+                    registrationForm.Approval = Approval.Text;
+                }
+                if (FirstName.Text != "")
+                {
+                    registrationForm.FirstName = FirstName.Text;
+                }
+                if (LastName.Text != "")
+                {
+                    registrationForm.LastName = LastName.Text;
+                }
+                if (UserName.Text != "")
+                {
+                    registrationForm.UserName = UserName.Text;
+                }
+                if (Email.Text != "")
+                {
+                    registrationForm.Email = Email.Text;
+                }
+                if (DateOfbirth.SelectedDate.Date != DateTime.MinValue)
+                {
+                    registrationForm.DateOfBirth = DateOfbirth.SelectedDate.Date;
+                }
+                if (Female.Text != "")
+                {
+                    registrationForm.Gender = true;
+                }
+                if (Male.Text != "")
+                {
+                    registrationForm.Gender = false;
+                }
+                if (List.SelectedItem.Value != "")
+                {
+                    registrationForm.PickList = List.SelectedItem.Value;
+                }
+            }
+            return registrationForm;
+
+        }
+        protected void RegisrationSubmit_Click(object sender, EventArgs e)
+        {
+            myAdoHelperAccess = new MyAdoHelperAccess();
+            RegistrationForm registrationForm = registaration();
+            var filename = string.Empty;
+            if (registrationForm.Password == null)
+            {
+                Response.Redirect("registration.aspx");
+            }
+            var sqlCommand = "INSERT INTO tbl_users (UserFirstName, UserLastName, BirthdatDate, UserName,UserPss,UserEmail)" +
+              "VALUES('" + registrationForm.FirstName.ToString() + "', '" + registrationForm.LastName.ToString() +
+              "', '" + registrationForm.DateOfBirth.ToString() + "','" + "', '" + registrationForm.UserName.ToString() +
+              "','" + registrationForm.Password.ToString() + "', '" + registrationForm.Email.ToString() + "')";
+            myAdoHelperAccess.DoQuery(filename,sqlCommand);
+
+
+        }
+        protected void GetUsersById(object sender, EventArgs e)
+        {
+            var filename = string.Empty;
+            myAdoHelperAccess = new MyAdoHelperAccess();
+            if (List.Text !="")
+            {
+                var sqlCommand = "SELECT * From tbl_users WHERE ID =" + List.Text;
+                DataTable userstbl =  myAdoHelperAccess.ExecuteDatatable(filename, sqlCommand);
+                foreach (DataRow row in userstbl.Rows)
+                {
+                    UserName.Text = row["UserName"].ToString();
+                    LastName.Text = row["LastName"].ToString();
+                    FirstName.Text = row["FirstName"].ToString();
+                    Email.Text = row["Email"].ToString();
+                }
+            }
+
+        }
+        protected void ResetButton_Click(object sender, EventArgs e)
+        {
+            if (Approval.Text != "")
+            {
+                Approval.Text = "";
+            }
+            if (FirstName.Text != "")
+            {
+                FirstName.Text = "";
+            }
+            if (LastName.Text != "")
+            {
+                LastName.Text = "";
+            }
+            if (UserName.Text != "")
+            {
+                UserName.Text = "";
+            }
+            if (Email.Text != "")
+            {
+                Email.Text = "";
+            }
+        }
+    }
+    partial class RegistrationForm
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string UserName { get; set; }
+        public string PickList { get; set; }
+        public string Password { get; internal set; }
+        public string Email { get; internal set; }
+        public bool Gender { get; internal set; }
+        public string Approval { get; internal set; }
+
+        public RegistrationForm() { }
+
+        public RegistrationForm(string FirstName, string UserName, int pageCount, DateTime DateOfBirth, string PickList)
+        {
+            this.UserName = UserName;
+            this.FirstName = FirstName;
+            this.DateOfBirth = DateOfBirth;
+            this.LastName = LastName;
+            this.PickList = PickList;
+        }
+
+    }
+}
